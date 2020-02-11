285b27c8bf6283173cd29c20a8b8c12c64d5cbc2
rajataggarwal1 <rajataggarwal2@gmail.com>
2020-02-10 23:09:08 -0800

    Importing CSS and HTML, Create UI and layout of application.
    Updating hear/index and app.js
    
    Branch15

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -5,10 +5,24 @@ import Header from "./Layout/Header";
 class Dashboard extends Component {
   render() {
     return (
-      <div>
-        <Header />
-        <h1>Welcome to dasdboard</h1>
-        <ProjectItem />
+      <div className="projects">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4 text-center">Projects</h1>
+              <br />
+              <a href="ProjectForm.html" className="btn btn-lg btn-info">
+                Create a Project
+              </a>
+              <br />
+              <hr />
+              {
+                // Importing project item to show the project items
+              }
+              <ProjectItem />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
